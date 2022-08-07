8c7cfd85fadb75d962b7efc519a99dce2f0a40f4
unknown <webpragramist@gmail.com>
2022-08-07 12:50:10 +0500

    changed

--- src/components/Ariza/index.jsx
+++ src/components/Ariza/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Container,
+  InpBox,
+  Input,
+  Select,
+  TextArea,
+  Title,
+  Wrapper,
+} from "./styled";
+import Navbar from "../../components/Navbar";
+import Footer from "../Footer";
 
-const Ariza = () => {
+const Taklif = () => {
   return (
-    <div>Ariza</div>
-  )
-}
+    <div className="block-center">
+      <Container>
+        <Wrapper>
+          <Title>
+            <div className="title">
+              Ariza<span className="blue">/</span> Shikoyat{" "}
+              <span className="blue">bo’limi</span>
+            </div>
+          </Title>
+          <InpBox>
+            <Input placeholder="Arizaga nom bering" type="text" />
+            <Select>
+              <option value="">Maktab tizimi</option>
+            </Select>
+            <TextArea placeholder="Yozing..." />
+            <div className="block-center">
+              <Button>Yuborish</Button>
+            </div>
+          </InpBox>
+        </Wrapper>
+      </Container>
+    </div>
+  );
+};
 
-export default Ariza
+export default Taklif;
