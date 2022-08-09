0a27f982c8e4cab5a218e9faec5f7084220690f5
unknown <webpragramist@gmail.com>
2022-08-09 15:55:03 +0500

    signin added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Root from './root';
 import './index.css';
+import SignIn from './pages/Signin'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <BrowserRouter>
-      <Root/>
+      <SignIn/>
     </BrowserRouter> 
   </>
 );
--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { Button, Container, Input, Line, Wrapper } from "./style";
 
 const Signin = () => {
   return (
-    <div>Signin</div>
-  )
-}
+    <Container>
+      <div className="block-center">
+        <Wrapper>
+          <div className="title">Login</div>
+          <Line></Line>
+          <div className="description">Xush kelibsiz!</div>
+          <div className="inputs">
+            <div className="description">Password</div>
+            <Input type={"password"}/>
+            <br /><br />
+            <div className="description">Username</div>
+            <Input />
+            <div className="des">Forgot Password?</div>
+          </div>
+          <Button>
+            <div className="block-center">LOGIN</div>
+          </Button>
+          <div className="center">
+            <span>Register </span>Here
+          </div>
+        </Wrapper>
+      </div>
+    </Container>
+  );
+};
 
-export default Signin
+export default Signin;
