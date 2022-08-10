fc8b4f514ed459b1ea70fce8853bf0b0d2e7469d
jumaniyozoff1 <jumaniyozov0202@gmail.com>
2022-08-10 10:07:53 +0500

    changed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import Root from './root';
 import './index.css';
+import Signin from './pages/Signin'
 
-
+const count = 5
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <BrowserRouter>
-      <Root/>
+    {
+      count === 5 ? <Root/> : <Signin/>
+    }
     </BrowserRouter> 
   </>
 );
