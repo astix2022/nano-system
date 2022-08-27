509a2bc6db17b15060beed12a86293798c171c83
unknown <webpragramist@gmail.com>
2022-08-27 17:09:41 +0500

    boldi

---
+++ src/Admin/components/Sozlamalar/UsersT/Card/style.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const TableTop = styled.div`
+  display: flex;
+  height: 50px;
+  .tit {
+    width: 180px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 21px;
+  }
+`;
+
+const Tabel = styled.div``;
+
+const TabelCard = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: fit-content;
+  background-color: #f5f8ff;
+  align-items: center;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  .tit {
+    width: 220px;
+    display: flex;
+    align-items: center;
+    height: 30px;
+  }
+  .pointer {
+    cursor: pointer;
+  }
+  .blue {
+    color: #1e5dfe;
+  }
+`;
+
+export { Container, TableTop, Tabel, TabelCard };
--- src/Admin/components/Takliflar/ListCard/index.jsx
+++ src/Admin/components/Takliflar/ListCard/index.jsx
@@ -24,15 +24,23 @@ const ListCard = ({info,onClick}) => {
   )
   return (
     <Container>
-      <Item className='idraqam'>{info?.id || 'ID 1235781'}</Item>
-      <Item className='ismFamilya'>{info?.Name || 'Mavluda Sultonova'}</Item>
-      <Item className='taklifNomi'>{info?.suggestName || 'Taklif nomi '}</Item>
-      <Item className='sana'>13.07.2022</Item>
-      <Item className='button'><button className='btn' onClick={onSubmit}>Click</button></Item>
-      <div className='button' ><button  className='btn_delate' onClick={onClick}>delate</button></div>
-      {state === true ?  Component : null}
+      <Item className="idraqam">{info?.id || "ID 1235781"}</Item>
+      <Item className="ismFamilya">{info?.Name || "Jurat Xudayberganov"}</Item>
+      <Item className="taklifNomi">{info?.suggestName || "Taklif nomi "}</Item>
+      <Item className="sana">{info?.sent_date.slice(0,9) || "13.07.2022"}</Item>
+      <Item className="button">
+        <button className="btn" onClick={onSubmit}>
+          Click
+        </button>
+      </Item>
+      <div className="button">
+        <button className="btn_delate" onClick={onClick}>
+          delate
+        </button>
+      </div>
+      {state === true ? Component : null}
     </Container>
-  )
+  );
 }
 
 export default ListCard
