636f2b1ed93ca01f0097157e9f7567445479e513
jumaniyozoff1 <jumaniyozov0202@gmail.com>
2022-09-01 18:47:46 +0500

    generic card for home added

---
+++ src/components/Asosiy/Boglanish/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    padding: 0 100px;
+    margin-top: 100px;
+    .title{
+        text-align: center;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 28px;
+        color: #000000;
+    }
+`
+
+export
+{
+    Container,
+
+} 
+    
--- src/components/Asosiy/index.jsx
+++ src/components/Asosiy/index.jsx
@@ -4,15 +4,18 @@ import Footer from '../Footer'
 import Ozgarishlar from './Ozgarishlar'
 import Yutuqlar from './Yutuqlar'
 import EngYangi from './EngYangi'
+import Boglanish from './Boglanish'
+import HomeCard from './HomeCard'
 
 const Asosiy = () => {
   return (
     <div>
-      <Home/>
+      <HomeCard/>
+      {/* <Home/>
       <EngYangi/>
       <Ozgarishlar/>
       <Yutuqlar/>
-      <Footer/>
+      <Footer/> */}
     </div>
   )
 }
