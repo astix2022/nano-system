76ca43ab1b2349f4ee2ca00e829fe71e1eecacbf
nodirbek888 <xudayberganov8888@gmail.com>
2022-09-02 19:16:20 +0500

    aloqa

--- src/components/Aloqa/index.jsx
+++ src/components/Aloqa/index.jsx
@@ -1,13 +1,77 @@
 import React from 'react'
-import Footer from '../Footer'
-import Home from './Home'
-
+import {Container,Wrapper,WrapLeft,WrapRight,Item,Icon} from './style'
 const Aloqa = () => {
   return (
-    <div>
-      <Home/>
-      <Footer/>
-    </div>
+    <Container>
+      <Wrapper>
+        <WrapLeft>
+        <div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=urganch 45-IMI&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://goo.gl/maps/Tg4TqJdNVekN1V1TA">embed google maps in website</a></div></div>
+        </WrapLeft>
+        <WrapRight>
+          <h1 className='wrap_title'>ALOQA MA’LUMOTLARI</h1>
+          <Item>
+          <div className='wrp_item'>
+            <p className='wrap_item_title'>Bizning manzilimiz:</p>
+            <p className='wrap_item_des'>Xorazm viloyati, Urganch tumani, Paxtakor qishloqi</p>
+          </div>
+          <div className='wrp_item'>
+            <p className='wrap_item_title'>Bizning email manzil:</p>
+            <a href="45-idumi@gmail.com">
+            <p className='wrap_item_des'>45-idumi@gmail.com</p>
+            </a>
+          </div>
+          <div className='wrp_item'>
+            <p className='wrap_item_title'>Bizning rasmiy telefon raqamlarimiz:</p>
+            <a href="tel:998931234567">
+            <p className='wrap_item_des'>+998 (93) 123 45 67,</p>
+            </a>
+            <a href="tel:998931234567">
+            <p className='wrap_item_des'>+998 (93) 123 45 67</p>
+            </a>
+          </div>
+          <div className='wrp_item'>
+            <p className='wrap_item_title'>Maktabimiz bosh direktori:</p>
+            <p className='wrap_item_des'>Qozoqov Egambergan</p>
+          </div>
+          <div className='wrp_item'>
+            <p className='wrap_item_title'>Maktab direktor o’rinbosari:</p>
+            <p className='wrap_item_des'>Xajiyev Xajibek</p>
+          </div>
+          <div className='wrp_item'>
+            <p className='wrap_item_title'>Bizning ijtimoiy tarmoqdagi sahifalarimiz:</p>
+            <div className='icons'>
+              <a href="#">
+                <Icon.Telegram/>
+              </a>
+              <a href="#">
+                <Icon.Instagram/>
+              </a>
+              <a href="#">
+                <Icon.Facebook />
+              </a>
+              <a href="#">
+                <Icon.Youtube/>
+              </a>
+            </div>
+          </div>
+          <div className='wrp_item_app'>
+            <p className='wrap_item_title'>Ilovalar hovolasi:</p>
+            <div className='icons_app'>
+              <a href="#">
+                <Icon.Play/>
+              </a>
+              <a href="#">
+                <Icon.App/>
+              </a>
+            </div>
+          </div>
+          <div className='footer_des'>
+            <p className='footer_des_title'>Sayt ASTIX Team tomonidan 45-IDUMI ga 5-sentabr 2022 da foydalanishga topshirildi va shu kundan beri foydalanilib kelinmo1qda.</p>
+          </div>
+          </Item>
+        </WrapRight>
+      </Wrapper>
+    </Container>
   )
 }
 
---
+++ src/components/Aloqa/style.js
@@ -0,0 +1,188 @@
+import styled from "styled-components"; 
+import { ReactComponent as telegram } from "../../assets/icons/telegram2.svg";
+import { ReactComponent as instagram } from "../../assets/icons/instagram2.svg";
+import { ReactComponent as facebook } from "../../assets/icons/facebook2.svg";
+import { ReactComponent as youtube } from "../../assets/icons/youtube2.svg";
+import { ReactComponent as app } from "../../assets/icons/appstore.svg";
+import { ReactComponent as play } from "../../assets/icons/google-play.svg";
+
+
+const Container = styled.div`
+    width: 100%;
+    height: fit-content;
+    padding: 0px 100px;
+    margin-top: 40px;
+
+    p{
+        margin: 0;
+        padding: 0;
+    }
+
+    @media screen and (max-width:885px) {
+        padding: 0px 50px;
+    }
+    @media screen and (max-width:784px) {
+        padding: 0px 50px;
+    }
+
+    @media screen and (max-width:571px) {
+       padding: 0px 10px;
+        }
+`
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+
+    @media screen and (max-width:1115px) {
+        flex-direction: column;
+    }
+
+`
+const WrapLeft = styled.div`
+    width: 40%;
+    height: 445px;
+
+    .mapouter{
+        width: 100%;
+        height: 100%;
+
+        .gmap_canvas{
+            width: 100%;
+            height: 100%;
+
+            @media screen and (max-width:784px) {
+                text-align: center;
+            }
+
+            .gmap_iframe{
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+
+    @media screen and (max-width:1115px) {
+        width: 100%;
+    }
+`
+const WrapRight = styled.div`
+    width: 60%;
+    height: 300px;
+    padding-left: 20px;
+    margin-left: 10px;
+
+    .wrap_title{
+        font-size: 26px;
+
+        @media screen and (max-width:504px) {
+            font-size: 22px;
+        }
+    }
+
+    @media screen and (max-width:1115px) {
+        width: 100%;
+        margin-top: 30px;
+        margin-left: 0px;
+        padding-left: 0px;
+    }
+
+    @media screen and (max-width:784px) {
+        text-align: center;
+    }
+`
+
+const Item = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    .wrp_item{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        gap: 10px;
+
+        @media screen and (max-width:784px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        }
+    }
+
+    .wrp_item_app{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        gap: 30px;
+
+        @media screen and (max-width:784px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        }
+    }
+
+    .wrap_item_title{
+        font-size: 18px;
+        color: rgba(0, 0, 0, 1);
+        @media screen and (max-width:504px) {
+            font-size: 15px;
+        }
+    }
+
+    .wrap_item_des{
+        font-size: 16px;
+        color: rgba(58, 58, 58, 1);
+
+        @media screen and (max-width:504px) {
+            font-size: 13px;
+        }
+    }
+
+    .icons{
+        display: flex;
+        gap: 10px;
+    }
+
+    .icons_app{
+        display: flex;
+        gap: 40px;
+    }
+
+    .footer_des{
+        width: 100%;
+        margin-top: 30px;
+        @media screen and (max-width:504px) {
+            padding: 0px 40px;
+        }
+    }
+    
+    .footer_des_title{
+        font-size: 15px;
+        color: rgba(0, 0, 0, 1);
+        margin-bottom: 20px;
+        @media screen and (max-width:504px) {
+            font-size: 12px;
+        }
+    }
+`
+
+const Icon = styled.div``
+Icon.Telegram =styled(telegram)`
+    width:30px;
+`
+Icon.Instagram =styled(instagram)`
+    width:30px;
+`
+Icon.Facebook =styled(facebook)`
+    width:30px;
+`
+Icon.Youtube =styled(youtube)`
+    width:30px;
+`
+
+Icon.Play =styled(play)``
+
+Icon.App =styled(app)``
+
+export {Container,Wrapper,WrapLeft,WrapRight,Item,Icon}
